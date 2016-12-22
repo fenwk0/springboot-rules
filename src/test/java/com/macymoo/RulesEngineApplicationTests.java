a571d52e5054ccf4e43dd9fb6b4d63119187ef70
fenwkk <fenwkk@gmail;.com>
2016-12-22 11:41:31 +0000

    Initial commit

---
+++ src/test/java/com/macymoo/RulesEngineApplicationTests.java
@@ -0,0 +1,16 @@
+package com.macymoo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RulesEngineApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
